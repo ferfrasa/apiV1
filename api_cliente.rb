a331e4e1cdc592017812ee9a8131f5552a9b5e72
doris <dianafoxernesto@gmail.com>
2018-08-18 17:33:34 -0500

    Cambios en el api

--- api_cliente.rb
+++ api_cliente.rb
@@ -1,15 +1,29 @@
+#puts "Ingrese su nombre"
+puts "Ingrese el cuerpo de la publicacion"
+#name =  gets.chomp 
+post =  gets.chomp 
 require 'faraday' 
 
 client = Faraday.new(url:'http://localhost:3000') do |config|
     config.adapter Faraday.default_adapter
+    config.token_auth('55365018690302f1436702b3eb96b484')
 end
 
 response = client.post do |req|
+    req.url '/api/v1/posts'#url para pasar el dato
+    req.headers['Content-Type']='application/json' #json poraplicairon
+    req.body = '{"user":{"body": "post"}}' #pasa parametros
+end 
+=begin
+response = client.post do |req|
     req.url '/api/v1/users'#url para pasar el dato
     req.headers['Content-Type']='application/json' #json poraplicairon
-    req.body = '{"user":{"name": "testuser"}}' #pasa parametros
+    req.body = '{"user":{"name": "'+name+'"}}' #pasa parametros
 end 
+=end
 
 require 'oj'
 puts Oj.load(response.body)  #imprima la respuesta de body
 puts response.status
+
+#55365018690302f1436702b3eb96b484
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,32 @@
 module Api
     module V1
         class PostsController < ApplicationController
+           include ActionController::HttpAuthentication::Token::ControllerMethods
+           before_action :authenticate, only: [:create,:destroy] 
            def show
                 @posts = Post.order('created_at DESC')
                  render json: @posts
-           end  
+           end
 
+           def create
+                @post = @user.post.new(post_params)
+                if @post.save
+                   render json: @post, status: :created
+                else
+                   render json: @post.errors , status: :unprocessable_entity
+                end      
+           end 
+           
+            private
+             def authenticate
+                authenticate_or_request_with_http_token do |token,options|
+                @user = User.find_by(token: token)
+             end
+            end 
+
+           def post_params
+            params.require(:post).permit(:body)
+        end
         end
     end
 end 
